4cdba6c2c19547705c29fd76bc3d8989450ce831
Christian Hermann <christian.hermann94@alice.de>
2022-10-30 12:09:06 +0100

    Local checkpoint Repo

--- global.r
+++ global.r
@@ -1,81 +1,81 @@
 rm(list = ls())
-options(
-  repos = c(CRAN = "https://mran.revolutionanalytics.com/snapshot/2022-04-07"),
-  browser = "C:/Program Files/Mozilla Firefox/firefox.exe",
-  shiny.launch.browser = .rs.invokeShinyWindowExternal,
-  rlib_downstream_check = FALSE,
-  lib = "glyphicon"
-)
+options(repos = c(CRAN = "https://mran.revolutionanalytics.com/snapshot/2022-04-07"),
+        shiny.launch.browser = .rs.invokeShinyWindowExternal,
+        rlib_downstream_check = FALSE,
+        lib="glyphicon")
 
-if ("ephys.WSI" %in% .packages()) detach("package:ephys.WSI", unload = TRUE)
+if("ephys.WSI" %in%  .packages()) detach("package:ephys.WSI", unload = TRUE)
 
 library(checkpoint)
-checkpoint("2022-04-07", scanForPackages = F)
+####Packages on NAS (Slow!)####
+#checkpoint("2022-04-07", scanForPackages = F,
+#           checkpoint_location = "U:/Installationsprogramme/R/detecIVe2")
+
+####Packages on local drive (fast)####
+checkpoint("2022-04-07", scanForPackages = F,
+           checkpoint_location = "C:/R_Checkpoint")
 
 
-#### Newest relase####
-VersionEphysWSI <- "2.1.3"
-#### Newest relase####
 
+
+####Install the most needed packages to run####
+
+if(!require(plotly)) install.packages("plotly")
+if(!require(shinyBS)) install.packages("shinyBS")
+if(!require(shinyalert)) install.packages("shinyalert")
+if(!require(shinybusy)) install.packages("shinybusy")
+if(!require(shinycustomloader)) install.packages("shinycustomloader")
+if(!require(shinyWidgets)) install.packages("shinyWidgets")
+if(!require(shinydashboard)) install.packages("shinydashboard")
+if(!require(shinydashboardPlus)) install.packages("shinydashboardPlus")
+if(!require(shinyjs)) install.packages("shinyjs")
+if(!require(shiny)) install.packages("shiny")
+if(!require(rstudioapi)) install.packages("rstudioapi")
+if(!require(tryCatchLog)) install.packages("tryCatchLog")
+if(!require(esquisse)) install.packages("esquisse")
+if(!require(scales)) install.packages("scales")
+if(!require(gtools)) install.packages("gtools")
+
+####Newest release####
+VersionEphysWSI = '2.1.3'
+####Newest release####
 path <- rstudioapi::getSourceEditorContext()$path
-path <- paste(strsplit(path, "/")[[1]][-length(strsplit(path, "/")[[1]])], collapse = "/")
+path <- paste(strsplit(path,"/")[[1]][-length(strsplit(path,"/")[[1]])], collapse = "/")
+
+if(!require(ephys.WSI)) install.packages(
+  paste0(path,"/","ephys.WSI_",VersionEphysWSI,".zip"),
+  repos = NULL,
+  type = "win.binary"
+)
+
+####Install the most needed packages to run####
 
 
-#### Install the most needed packages to run####
-
-if (!require(plotly)) install.packages("plotly")
-if (!require(shinyBS)) install.packages("shinyBS")
-if (!require(shinyalert)) install.packages("shinyalert")
-if (!require(shinybusy)) install.packages("shinybusy")
-if (!require(shinycustomloader)) install.packages("shinycustomloader")
-if (!require(shinyWidgets)) install.packages("shinyWidgets")
-if (!require(shinydashboard)) install.packages("shinydashboard")
-if (!require(shinydashboardPlus)) install.packages("shinydashboardPlus")
-if (!require(shinyjs)) install.packages("shinyjs")
-if (!require(shiny)) install.packages("shiny")
-if (!require(rstudioapi)) install.packages("rstudioapi")
-if (!require(tryCatchLog)) install.packages("tryCatchLog")
-if (!require(esquisse)) install.packages("esquisse")
-if (!require(scales)) install.packages("scales")
-if (!require(gtools)) install.packages("gtools")
-
-if (!require(ephys.WSI)) {
+####Autoupdate Package####
+if (packageVersion("ephys.WSI") != VersionEphysWSI){
+  if("ephys.WSI" %in% (.packages()))
+    detach("package:ephys.WSI", unload = TRUE)
+  remove.packages("ephys.WSI", lib = "~/R/win-library/3.6")
   install.packages(
-    paste0(path, "/", "ephys.WSI_", VersionEphysWSI, ".zip"),
+    paste0(path,"/","ephys.WSI_",VersionEphysWSI,".zip"),
     repos = NULL,
     type = "win.binary"
   )
 }
 
-#### Install the most needed packages to run####
-
-### Envirs###
+####Autoupdate Package####
+###Envirs###
 data_storage_envir <- data_storage_envir
 data_envir <- data_envir
 settings_envir <- settings_envir
-### Envirs###
+###Envirs###
 
-#### Autoupdate Package####
-if (packageVersion("ephys.WSI") != VersionEphysWSI) {
-  if ("ephys.WSI" %in% (.packages())) {
-    detach("package:ephys.WSI", unload = TRUE)
-  }
-  remove.packages("ephys.WSI", lib = "~/R/win-library/3.6")
-  install.packages(
-    paste0(path, "/", "ephys.WSI_", VersionEphysWSI, ".zip"),
-    repos = NULL,
-    type = "win.binary"
-  )
-}
-#### Autoupdate Package####
-
-
-#### Support Packages List####
+####Support Packages List####
 supportPackages <- c(
   "dplyr",
   "tidyverse",
-  "vroom",
   "stringi",
+  "vroom",
   "openxlsx",
   "gWidgets2",
   "matrixStats",
@@ -98,34 +98,27 @@ supportPackages <- c(
   "Cairo",
   "gridExtra",
   "gWidgets2tcltk",
-  "ggpubr"
-)
+  "ggpubr")
 
-##### Global Variables#####
+#####Global Variables#####
 isDataImported <- FALSE
-colorPalette <- c(
-  "#000000", "#56B4E9", "#009E73",
-  "#F0E442", "#0072B2", "#D55E00",
-  "#CC79A7", "#999999", "#E69F00"
-)
-colorChoices <- list(
-  Boxplot = colorPalette,
-  Ratioplot = colorPalette,
-  SingleTraces = colorPalette,
-  MatTraces = colorPalette,
-  MedianTraces = colorPalette,
-  PPlot = colorPalette,
-  StitchedPlot = colorPalette
-)
-colorSelected <- list(
-  Boxplot = colorPalette,
-  Ratioplot = colorPalette,
-  SingleTraces = colorPalette,
-  MatTraces = colorPalette,
-  MedianTraces = colorPalette,
-  PPlot = colorPalette,
-  StitchedPlot = colorPalette
-)
+colorPalette <- c("#000000", "#0072B2", "#009E73",
+                  "#D55E00", "#56B4E9", #F0E442",
+                  "#CC79A7", "#999999", "#E69F00")
+colorChoices <- list(Boxplot = colorPalette,
+                     Ratioplot = colorPalette,
+                     SingleTraces = colorPalette,
+                     MatTraces = colorPalette,
+                     MedianTraces = colorPalette,
+                     PPlot = colorPalette,
+                     StitchedPlot = colorPalette)
+colorSelected <- list(Boxplot = colorPalette,
+                      Ratioplot = colorPalette,
+                      SingleTraces = colorPalette,
+                      MatTraces = colorPalette,
+                      MedianTraces = colorPalette,
+                      PPlot = colorPalette,
+                      StitchedPlot = colorPalette)
 
 IV_columns <- c(
   "None",
@@ -138,11 +131,11 @@ IV_columns <- c(
 
 
 axisChoices <- function(name) {
-  if (invalid(name)) {
-    return()
-  }
 
-  return(switch(name,
+  if(invalid(name)) return()
+
+  return(switch(
+    name,
     "None" = {
       NULL
     },
@@ -155,32 +148,25 @@ axisChoices <- function(name) {
     "IV-Analysis" = {
       IV_plot_axes_analysis
     },
+
     "P-Values" = {
       p_plot_axes
     },
     "P-Plot" = {
-      c(
-        IV_plot_axes,
-        p_plot_axes
-      )
+      c(IV_plot_axes,
+        p_plot_axes)
     },
     "P-Plot-Analysis" = {
-      c(
-        IV_plot_axes_analysis,
-        p_plot_axes_analysis
-      )
+      c(IV_plot_axes_analysis,
+        p_plot_axes_analysis)
     },
     "Complete-Plot" = {
-      c(
-        IV_plot_axes,
-        p_plot_axes
-      )
+      c(IV_plot_axes,
+        p_plot_axes)
     },
     "Complete-Plot-Analysis" = {
-      c(
-        IV_plot_axes_analysis,
-        p_plot_axes_analysis
-      )
+      c(IV_plot_axes_analysis,
+        p_plot_axes_analysis)
     },
     {
       input$Used_Axis
@@ -189,7 +175,8 @@ axisChoices <- function(name) {
 }
 
 themeChoices <- function(name) {
-  return(switch(name,
+  return(switch(
+    name,
     "Boxplot" = {
       theme_chris_boxplot
     },
@@ -203,64 +190,52 @@ themeChoices <- function(name) {
       c(theme_chris_IV, theme_chris_P_values)
     },
     "P-Plot-Analysis" = {
-      c(
-        theme_chris_IV_analysis,
-        theme_chris_P_values_analysis
-      )
+      c(theme_chris_IV_analysis,
+        theme_chris_P_values_analysis)
     },
     "Complete-Plot" = {
       c(theme_chris_IV, theme_chris_P_values)
     },
     "Complete-Plot-Analysis" = {
-      c(
-        theme_chris_IV_analysis,
-        theme_chris_P_values_analysis
-      )
+      c(theme_chris_IV_analysis,
+        theme_chris_P_values_analysis)
     }
   ))
 }
 
 
 
-plot_list <- list(
-  Boxplot = NULL,
-  Ratioplot = NULL,
-  SingleTraces = NULL,
-  MatTraces = NULL,
-  MedianTraces = NULL,
-  PPlot = NULL,
-  StitchedPlot = NULL
-)
+plot_list <- list(Boxplot = NULL,
+                  Ratioplot = NULL,
+                  SingleTraces = NULL,
+                  MatTraces = NULL,
+                  MedianTraces = NULL,
+                  PPlot = NULL,
+                  StitchedPlot = NULL)
 
-plot_list_woLegend <- list(
-  Boxplot = NULL,
-  Ratioplot = NULL,
-  SingleTraces = NULL,
-  MatTraces = NULL,
-  MedianTraces = NULL,
-  PPlot = NULL,
-  StitchedPlot = NULL
-)
+plot_list_woLegend <- list(Boxplot = NULL,
+                           Ratioplot = NULL,
+                           SingleTraces = NULL,
+                           MatTraces = NULL,
+                           MedianTraces = NULL,
+                           PPlot = NULL,
+                           StitchedPlot = NULL)
 
-legend_list <- list(
-  Boxplot = NULL,
-  Ratioplot = NULL,
-  SingleTraces = NULL,
-  MatTraces = NULL,
-  MedianTraces = NULL,
-  PPlot = NULL,
-  StitchedPlot = NULL
-)
+legend_list <- list(Boxplot = NULL,
+                    Ratioplot = NULL,
+                    SingleTraces = NULL,
+                    MatTraces = NULL,
+                    MedianTraces = NULL,
+                    PPlot = NULL,
+                    StitchedPlot = NULL)
 
-dimensions_list <- list(
-  Boxplot = NULL,
-  Ratioplot = NULL,
-  SingleTraces = NULL,
-  MatTraces = NULL,
-  MedianTraces = NULL,
-  PPlot = NULL,
-  StitchedPlot = NULL
-)
+dimensions_list <- list(Boxplot = NULL,
+                        Ratioplot = NULL,
+                        SingleTraces = NULL,
+                        MatTraces = NULL,
+                        MedianTraces = NULL,
+                        PPlot = NULL,
+                        StitchedPlot = NULL)
 
 
 
@@ -331,8 +306,6 @@ windowsFonts("Verdana" = windowsFont("Verdana"))
 
 
 
-Fonts <- c(
-  "Arial", "Bahnschrift", "Cambria Math", "Comic Sans", "Courier New",
-  "Helvetica", "Palatino Linotype", "SimSun", "Times New Roman",
-  "Verdana", "Webdings", "Yu Gothic"
-)
+Fonts <- c("Arial", "Bahnschrift", "Cambria Math", "Comic Sans", "Courier New",
+           "Helvetica", "Palatino Linotype", "SimSun","Times New Roman",
+           "Verdana", "Webdings", "Yu Gothic")
