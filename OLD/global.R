b9058600847f258ad506857add86116e8b69eb2b
Christian Hermann <christian.hermann94@alice.de>
2022-11-13 23:13:07 +0100

    deleted some old stuff

--- OLD/global.R
+++
@@ -1,5 +0,0 @@
-enableBookmarking(store = "server")
-
-#options(browser = "C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
-options(shiny.launch.browser = .rs.invokeShinyWindowExternal)
-#runApp('U:/Projekte an Analysis1/Christian/R-Scripts for everybody/detectIVe (Summary_IV_evaluation)', launch.browser = T)
